1c8e3e0bc7ff26283632d20a27f1b2c7592e0ccb
azofeifa <jgazofeifa@gmail.com>
2016-01-22 12:40:15 -0700

    updates

--- qsub_scripts/run_observation.sh
+++ qsub_scripts/run_observation.sh
@@ -22,14 +22,15 @@ module load mpich_3.1.4
 #paths to config and src
 src=/Users/azofeifa/Lab/gTFIv2/CPP_src/SE
 DB=/Users/azofeifa/Lab/gTFIv2/PSSM_DB/HOCOMOCOv10_HUMAN_mono_meme_format.meme
-bed=/Users/azofeifa/Lab/gro_seq_files/Allen2014/EMG_out_files/before_1_1/Allen2014_Nutlin2_3-3_divergent_classifications.bed
+bed=/Users/azofeifa/Lab/gro_seq_files/Allen2014/EMG_out_files/before_1_1/Allen2014_DMSO2_3-19_divergent_classifications.bed
 fasta=/Users/azofeifa/Lab/genome_files/hg19.fa
 out=/Users/azofeifa/
-br=500
-pad=1500
+br=100
+pv=0.0000001
 log_out=/Users/azofeifa/
+ID=DMSO
 #================================================================
 #calling command
 cmd="mpirun -np $PBS_NUM_NODES -hosts ${hostlist}"
-$cmd $src -DB $DB -fasta $fasta -bed $bed -o $out -br $br -pad $pad -log_out $log_out
+$cmd $src -DB $DB -fasta $fasta -bed $bed -o $out -br $br -pv $pv -log_out $log_out -ID $ID
 #================================================================
