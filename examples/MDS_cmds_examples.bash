a868c478d3798ddd47f68662c86b66dcedc019cf
azofeifa <jgazofeifa@gmail.com>
2017-03-29 09:21:03 -0600

    checked the database file output

--- examples/MDS_cmds_examples.bash
+++ examples/MDS_cmds_examples.bash
@@ -16,12 +16,11 @@ log_out=${install_location}/MDS/examples/
 
 ID=unit_test_run_DB
 ID2=unit_test_run_EVAL
-NP=1
+NP=4
 sim_N=100
 pv=0.000001
 bsn=150
 H=1500
-h=150
 
 
 echo '-------------------------Running Unit Tests-------------------------'
@@ -31,11 +30,11 @@ echo 'Installation Location: ' $install_location ' is this correct?'
 
 echo '-------------------------Running DB Module--------------------------'
 
-#mpirun -np $NP $src DB -bed $input_interval_file -fasta $fasta_file -DB $PSSM_DB -o $out_file_DB_dir -log_out $log_out -ID $ID -sim_N $sim_N -pv $pv -h $h -H $H  
-#wait
+mpirun -np $NP $src DB -bed $input_interval_file -fasta $fasta_file -DB $PSSM_DB -o $out_file_DB_dir -log_out $log_out -ID $ID -sim_N $sim_N -pv $pv  -H $H  -t 10
+wait
 echo '-------------------------Running EVAL Module------------------------'
 
-mpirun -np $NP $src EVAL -bed $input_interval_file -fasta $fasta_file -DB $PSSM_DB -o $out_file_stats_dir -log_out $log_out -ID $ID2 -bsn $bsn -h $h -H $H -pv $pv -t 3
+#mpirun -np $NP $src EVAL -bed $input_interval_file -fasta $fasta_file -DB $PSSM_DB -o $out_file_stats_dir -log_out $log_out -ID $ID2 -bsn $bsn  -H $H -pv $pv -t 3
 
 
 
